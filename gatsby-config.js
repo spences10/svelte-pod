c838ade824b4b9b25c3b6c76ff32b1227e18e601
spences10 <spences10apps@gmail.com>
2018-10-29 11:25:58 +0000

    move comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,8 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png',
       },
     },
     'gatsby-plugin-offline',
