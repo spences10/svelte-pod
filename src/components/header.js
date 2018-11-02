d34599e307064593e61b084f1b65237d9ac2f8c8
spences10 <spences10apps@gmail.com>
2018-11-02 15:52:42 +0000

    use styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  grid-area: header;
+  background: #0a111e;
+  margin-bottom: 1.45rem;
+
+  display: grid;
+`
+
+const SiteTitle = styled.h1`
+  color: #fff;
+  margin: 0;
+`
+
+const SiteLink = styled(Link)`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  color: white;
+  text-decoration: 'none';
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <StyledHeader>
+    {/* <Image img={'logo.png'} /> */}
+    <SiteLink to="/">
+      <SiteTitle>{siteTitle}</SiteTitle>
+    </SiteLink>
+  </StyledHeader>
 )
 
 export default Header
