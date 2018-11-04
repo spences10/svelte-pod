70680edb8a9c5b5be9ddf2a7aab910a8c73a3b0e
spences10 <spences10apps@gmail.com>
2018-11-04 11:28:24 +0000

    add rss query to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,78 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
+// import Dump from '../components/dump'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const sortPodcasts = podcastArr =>
+  podcastArr.reduce((acc, podcast) => {
+    const image = podcast.rss2Feed.image
+    const title = podcast.rss2Feed.title
+    const items = podcast.rss2Feed.items
+
+    return [
+      ...acc,
+      ...items.map(item => ({
+        ...item,
+        podcast: {
+          title,
+          image
+        }
+      }))
+    ].sort((a, b) => b.pubDate - a.pubDate)
+  }, [])
+
+const IndexPage = ({ data }) => {
+  const items = sortPodcasts([data.oneGraph.jsr, data.oneGraph.fehh])
+  return (
+    <Layout>
+      {/* <Dump data={data} /> */}
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} style={{ color: 'white' }}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    oneGraph {
+      jsr: rss {
+        rss2Feed(
+          url: "https://www.heavybit.com/category/library/podcasts/jamstack-radio/feed/"
+        ) {
+          ...RSSFragment
+        }
+      }
+      fehh: rss {
+        rss2Feed(
+          url: "http://feeds.soundcloud.com/users/soundcloud:users:206137365/sounds.rss"
+        ) {
+          ...RSSFragment
+        }
+      }
+    }
+  }
+  fragment RSSFragment on ONEGRAPH_Rss2Channel {
+    image {
+      uri
+    }
+    title
+    items {
+      pubDate
+      title
+      link
+      enclosure {
+        mime
+        url
+        length
+      }
+    }
+  }
+`
