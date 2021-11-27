f124bfcab079fee0d4db3027f5fb18c8a29a833e
spences10 <spences10apps@gmail.com>
2021-11-27 16:39:28 +0000

    chore: :wrench: add more pods

--- src/lib/podcasts.js
+++ src/lib/podcasts.js
@@ -1,10 +1,22 @@
 export const podList = [
   {
+    title: `Svelte Radio`,
+    url: `https://feeds.transistor.fm/svelte-radio`,
+  },
+  {
     title: `Darknet Diaries`,
     url: `https://feeds.megaphone.fm/darknetdiaries`,
   },
   {
     title: `Land of the Giants`,
     url: `https://feeds.megaphone.fm/landofthegiants`,
+  },
+  {
+    title: `Smashing Security`,
+    url: `https://feeds.fireside.fm/smashingsecurity/rss`,
+  },
+  {
+    title: `Grumpy Old Geeks`,
+    url: `https://rss.art19.com/grumpy-old-geeks-podcast`,
   },
 ]
