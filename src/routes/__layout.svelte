dca83613d94623f6960fc536bf5dd857fcf8e806
spences10 <spences10apps@gmail.com>
2021-11-28 18:54:03 +0000

    feat: :sparkles: use store for index list

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script>
+  import Player from '$lib/components/player.svelte'
   import '../app.css'
+  import { nowPlaying } from '../stores/now-playing'
 </script>
 
-<slot />
+<Player url={$nowPlaying} />
+
+<main class="container max-w-xl mx-auto px-4">
+  <slot />
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,19 +11,41 @@
 </script>
 
 <script>
-  import slugify from 'slugify'
+  import { podcastsStore } from '../stores/podcasts'
 
-  export let podcasts
+  // export let podcasts
 </script>
 
-{#each podcasts as { title }}
+<!-- <pre>{JSON.stringify($podcastsStore, null, 2)}</pre> -->
+
+{#each $podcastsStore as { title, copyright, description, image }}
   <ul>
-    <li>
-      <a
-        sveltekit:prefetch
-        href={`/podcasts/${slugify(title, { lower: true })}`}
-        >{title}</a
-      >
+    <li class="py-10">
+      <div class="card bordered">
+        <figure>
+          {#if image?.uri}
+            <img src={image.uri} alt="" />
+          {:else}
+            <img
+              src="https://api.lorem.space/image/fashion?w=500&h=500"
+              alt={title}
+            />
+          {/if}
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">
+            {title}
+            <div class="badge mx-2 badge-secondary">NEW</div>
+          </h2>
+          <small class="mb-5 -mt-2">{copyright}</small>
+          <p>
+            {@html description}
+          </p>
+          <div class="justify-end card-actions">
+            <button class="btn btn-secondary">More info</button>
+          </div>
+        </div>
+      </div>
     </li>
   </ul>
 {/each}
