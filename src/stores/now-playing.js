50b3bf088c8fe3fa0433353584ea8051f89947d9
spences10 <spences10apps@gmail.com>
2021-11-29 11:32:04 +0000

    feat: :sparkles: add set current time

--- src/stores/now-playing.js
+++ src/stores/now-playing.js
@@ -1,7 +1,12 @@
 import { writable } from 'svelte/store'
 
 export const nowPlaying = writable(``)
+export const currentTime = writable(0)
 
 export const setNowPlaying = url => {
   nowPlaying.set(url)
+}
+
+export const setCurrentTime = currentTime => {
+  nowPlaying.set(currentTime)
 }
