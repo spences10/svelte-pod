f03fdcbdf9bfb8a31597b0fd00740ed273081bb5
spences10 <spences10apps@gmail.com>
2021-11-29 11:33:22 +0000

    feat: :sparkles: additional controls and progress bar

--- src/lib/components/player.svelte
+++ src/lib/components/player.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { onMount } from 'svelte'
+  import { onDestroy, onMount } from 'svelte'
   import {
     nowPlaying,
     setCurrentTime,
@@ -8,21 +8,70 @@
   export let url
 
   let audioFile
+  let isPlaying = false
+  let vol = 50
+  let totalTrackTime
+  let totalTimeDisplay = 'loading...'
+  let currentTimeDisplay = '0:00:00'
+  let progress
+  let trackTimer
 
   onMount(() => {
     audioFile = new Audio(url)
-  })
-  // onDestroy(() => {
-  //   audioFile = null
-  // })
 
-  let isPlaying = false
+    audioFile.onloadedmetadata = () => {
+      totalTrackTime = audioFile.duration
+      updateTime()
+    }
+  })
+
+  const updateTime = () => {
+    progress = audioFile.currentTime * (100 / totalTrackTime)
+
+    let currentHours = Math.floor(audioFile.currentTime / 60 / 60)
+    let currentMinuets = Math.floor(audioFile.currentTime / 60)
+    let currentSeconds = Math.floor(
+      audioFile.currentTime - currentMinuets * 60
+    )
+
+    let durationHours = Math.floor(totalTrackTime / 60 / 60)
+    let durationMinutes = Math.floor((totalTrackTime / 60) % 60)
+    let durationSeconds = Math.floor(
+      totalTrackTime - durationHours * 60 * 60 - durationMinutes * 60
+    )
+
+    if (currentSeconds < 10) currentSeconds = `0${currentSeconds}`
+    if (durationSeconds < 10) durationSeconds = `0${durationSeconds}`
+    if (currentMinuets < 10) currentMinuets = `0${currentMinuets}`
+    if (durationMinutes < 10) durationMinutes = `0${durationMinutes}`
+
+    currentTimeDisplay = `${currentHours}:${currentMinuets}:${currentSeconds}`
+    totalTimeDisplay = `${durationHours}:${durationMinutes}:${durationSeconds}`
+
+    if (audioFile.ended) {
+      toggleTimeRunning()
+    }
+  }
+
+  const toggleTimeRunning = () => {
+    if (audioFile.ended) {
+      clearInterval(trackTimer)
+    } else {
+      trackTimer = setInterval(updateTime, 100)
+    }
+  }
+
+  onDestroy(() => {
+    audioFile = null
+  })
 
   const playAudio = () => {
     if (audioFile.paused) {
+      toggleTimeRunning()
       audioFile.play()
       isPlaying = true
     } else {
+      toggleTimeRunning()
       audioFile.pause()
       isPlaying = false
     }
@@ -38,9 +87,18 @@
     setCurrentTime(audioFile.currentTime)
   }
 
-  let vol = 50
   const adjustVol = () => {
     audioFile.volume = vol / 100
+  }
+
+  // increase playback rate
+  const increasePlaybackRate = () => {
+    audioFile.playbackRate += 0.1
+  }
+
+  // decrease playback rate
+  const decreasePlaybackRate = () => {
+    audioFile.playbackRate -= 0.1
   }
 </script>
 
@@ -67,5 +125,23 @@
       on:input={adjustVol}
     />
     <span>{vol}</span>
+  </div>
+
+  <div>
+    <span>{currentTimeDisplay}</span>
+    <span>{totalTimeDisplay}</span>
+  </div>
+
+  <div
+    class="w-full h-3 flex items-center border border-dark-500 cursor-pointer"
+  >
+    <span class="h-3 bg-primary" style="width: {progress}%" />
+  </div>
+
+  <div>
+    <button on:click={increasePlaybackRate}>+ Playback Rate</button>
+  </div>
+  <div>
+    <button on:click={decreasePlaybackRate}>- Playback Rate</button>
   </div>
 </section>
