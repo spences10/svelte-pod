c4e414a791c037ed5f51758bacd0cb68cdde316d
spences10 <spences10apps@gmail.com>
2021-11-29 10:23:45 +0000

    feat: :sparkles: add player controls

--- src/lib/components/player.svelte
+++ src/lib/components/player.svelte
@@ -1,5 +1,71 @@
 <script>
+  import { onMount } from 'svelte'
+  import {
+    nowPlaying,
+    setCurrentTime,
+  } from '../../stores/now-playing'
+
   export let url
+
+  let audioFile
+
+  onMount(() => {
+    audioFile = new Audio(url)
+  })
+  // onDestroy(() => {
+  //   audioFile = null
+  // })
+
+  let isPlaying = false
+
+  const playAudio = () => {
+    if (audioFile.paused) {
+      audioFile.play()
+      isPlaying = true
+    } else {
+      audioFile.pause()
+      isPlaying = false
+    }
+  }
+
+  const rewindAudio = () => {
+    audioFile.currentTime -= 10
+    setCurrentTime(audioFile.currentTime)
+  }
+
+  const forwardAudio = () => {
+    audioFile.currentTime += 10
+    setCurrentTime(audioFile.currentTime)
+  }
+
+  let vol = 50
+  const adjustVol = () => {
+    audioFile.volume = vol / 100
+  }
 </script>
 
-<audio src={url} controls />
+<!-- https://www.youtube.com/watch?v=4rJ1F-tOk-M -->
+
+<section class="fixed bottom-0 left-0 right-0 bg-red-500">
+  {$nowPlaying}
+  <div><button on:click={rewindAudio}>Rewind</button></div>
+  <div>
+    <button on:click={playAudio}
+      >{isPlaying ? `Pause` : `Play`}</button
+    >
+  </div>
+  <div><button on:click={forwardAudio}>Forward</button></div>
+  <!-- <audio src={url} controls /> -->
+  <div>
+    <span>0</span>
+    <input
+      type="range"
+      min="0"
+      max="100"
+      step="1"
+      bind:value={vol}
+      on:input={adjustVol}
+    />
+    <span>{vol}</span>
+  </div>
+</section>
