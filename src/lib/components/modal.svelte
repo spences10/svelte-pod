00eb3d636ba402d799e1b5b04b3bb46c48e404d1
spences10 <spences10apps@gmail.com>
2021-11-29 08:07:32 +0000

    feat: :sparkles: add modal

---
+++ src/lib/components/modal.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { fade, fly } from 'svelte/transition'
+  import Portal from './portal.svelte'
+
+  export let isModalOpen = false
+
+  function closeModal() {
+    isModalOpen = false
+  }
+</script>
+
+{#if isModalOpen}
+  <Portal>
+    <div transition:fly={{ opacity: 0, y: 100 }}>
+      <button on:click={closeModal} aria-label="close modal"
+        >Close</button
+      >
+      <slot />
+    </div>
+    <div on:click={closeModal} transition:fade />
+  </Portal>
+{/if}
---
+++ src/lib/components/portal.svelte
@@ -0,0 +1,28 @@
+<script>
+  function portal(node) {
+    let target
+
+    function update() {
+      target = document.querySelector('body')
+      target.appendChild(node)
+      node.hidden = false
+    }
+
+    function destroy() {
+      if (node.parantNode) {
+        node.parantNode.removeChild(node)
+      }
+    }
+
+    update()
+
+    return {
+      update,
+      destroy,
+    }
+  }
+</script>
+
+<div use:portal hidden>
+  <slot />
+</div>
