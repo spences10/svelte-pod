d64659a5bf5cc082da6817939983d4c4de1c23dd
spences10 <spences10apps@gmail.com>
2021-12-01 07:20:34 +0000

    feat: :bento: add player icons
    https://icones.js.org/collection/carbon

---
+++ src/lib/icons/pause.svelte
@@ -0,0 +1,18 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  aria-hidden="true"
+  role="img"
+  class="iconify iconify--carbon"
+  width="32"
+  height="32"
+  preserveAspectRatio="xMidYMid meet"
+  viewBox="0 0 32 32"
+  ><path d="M14 10h-2v12h2V10z" fill="currentColor" /><path
+    d="M20 10h-2v12h2V10z"
+    fill="currentColor"
+  /><path
+    d="M16 4A12 12 0 1 1 4 16A12 12 0 0 1 16 4m0-2a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"
+    fill="currentColor"
+  /></svg
+>
---
+++ src/lib/icons/play.svelte
@@ -0,0 +1,18 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  aria-hidden="true"
+  role="img"
+  class="iconify iconify--carbon"
+  width="32"
+  height="32"
+  preserveAspectRatio="xMidYMid meet"
+  viewBox="0 0 32 32"
+  ><path
+    d="M11 23a1 1 0 0 1-1-1V10a1 1 0 0 1 1.447-.894l12 6a1 1 0 0 1 0 1.788l-12 6A1.001 1.001 0 0 1 11 23zm1-11.382v8.764L20.764 16z"
+    fill="currentColor"
+  /><path
+    d="M16 4A12 12 0 1 1 4 16A12 12 0 0 1 16 4m0-2a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"
+    fill="currentColor"
+  /></svg
+>
